fdb164ea7cf07992a274ffc689c8d46b43ef8d66
VirtusXVI <manuel.aveta2001@gmail.com>
2022-08-23 15:02:27 +0200

    completed exercise

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/1', function () {
+
+    $data = [
+        'points' => ['Home','About','Docs','Laracasts','News','Blog','Nova','Forge'],
+    ];
+
+    return view('home', $data);
+});
+
+Route::get('/2', function () {
     return view('welcome');
 });
